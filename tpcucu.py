355f32989e512de9e1119984cd0f93c3f7e33d12
cabragg <hansidietrich06@gmail.com>
2024-06-29 19:23:52 -0400

    Create tpcucu.py

---
+++ tpcucu.py
@@ -0,0 +1,7 @@
+pesos = float(input ("Ingresar monto en pesos argentinos:"))  
+cambio_oficial = 1200
+blue = 1400
+exchange = pesos / cambio_oficial
+black_ex = pesos / blue
+print ("Equivalente a" , exchange , "Dollar oficial")
+print ("Equivalente a" , black_ex , "Dollar blue")
